ed2c9d570272c91fcd48054dde1ba2acd3d9ac8f
Mehmet Can Taş <tass.mehmetcan@outlook.com>
2021-02-24 01:49:15 +0300

    Add builder pattern for customer and payment request

--- models/customer.go
+++ models/customer.go
@@ -11,3 +11,43 @@ type Customer struct {
 	IsCompany              bool
 	CustomerID             string
 }
+
+type CustomerBuilder struct {
+	customer *Customer
+}
+
+func NewCustomerBuilder() *CustomerBuilder {
+	return &CustomerBuilder{&Customer{}}
+}
+
+func (c *CustomerBuilder) NameIs(name string) *CustomerBuilder {
+	c.customer.FullName = name
+	return c
+}
+func (c *CustomerBuilder) EmailIs(email string) *CustomerBuilder {
+	c.customer.EmailAddress = email
+	return c
+}
+func (c *CustomerBuilder) ShipTo(sAddress string, sZipCode string) *CustomerBuilder {
+	c.customer.ShippingAddress = sAddress
+	c.customer.ShippingAddressZipCode = sZipCode
+	return c
+}
+
+func (c *CustomerBuilder) BillTo(bAddress string, bZipCode string) *CustomerBuilder {
+	c.customer.BillingAddress = bAddress
+	c.customer.BillingAddressZipCode = bZipCode
+	return c
+}
+func (c *CustomerBuilder) IpAddress(ipAddress string) *CustomerBuilder {
+	c.customer.IPAddress = ipAddress
+	return c
+}
+func (c *CustomerBuilder) IsCompany(isCompany bool) *CustomerBuilder {
+	c.customer.IsCompany = isCompany
+	return c
+}
+func (c *CustomerBuilder) WithID(id string) *CustomerBuilder {
+	c.customer.CustomerID = id
+	return c
+}
--- models/payment_gateway_request.go
+++ models/payment_gateway_request.go
@@ -1,18 +1,79 @@
 package models
 
-type PaymentGatewayRequest struct {
-	CardHolderName   string
-	CardNumber       string
-	CVV              string
-	ExpireMonth      string
-	ExpireYear       string
-	OrderNumber      string
-	OrderTotal       float64
-	InstallmentCount int
-	CurrencyCode     string
-	LanguageCode     string
-	CardType         string
-	SuccessURL       string
-	FailURL          string
-	Customer         *Customer
+import (
+	"github.com/mehmetcantas/gopos/models/card_type"
+	"github.com/mehmetcantas/gopos/models/currency"
+)
+
+type PaymentRequest struct {
+	CardHolderName, CardNumber, CVV, ExpireMonth, ExpireYear string
+	OrderNumber                                              string
+	OrderTotal                                               float64
+	InstallmentCount                                         int
+	LanguageCode                                             string
+	SuccessURL, FailURL                                      string
+	CurrencyCode                                             currency.CurrencyCode
+	CardType                                                 card_type.CardType
+	Customer                                                 *Customer
+}
+
+type PaymentRequestBuilder struct {
+	PaymentRequest *PaymentRequest
+}
+
+func NewPaymentRequestBuilder() *PaymentRequestBuilder {
+	return &PaymentRequestBuilder{&PaymentRequest{}}
+}
+
+func (p *PaymentRequestBuilder) Card(holderName string, cardNumber string, CVV string) *PaymentRequestBuilder {
+	p.PaymentRequest.CardHolderName = holderName
+	p.PaymentRequest.CardNumber = cardNumber
+	p.PaymentRequest.CVV = CVV
+	return p
+}
+
+func (p *PaymentRequestBuilder) IsExpire(expireMonth string, expireYear string) *PaymentRequestBuilder {
+	p.PaymentRequest.ExpireMonth = expireMonth
+	p.PaymentRequest.ExpireYear = expireYear
+	return p
+}
+func (p *PaymentRequestBuilder) Type(cardType card_type.CardType) *PaymentRequestBuilder {
+	p.PaymentRequest.CardType = cardType
+	return p
+}
+
+func (p *PaymentRequestBuilder) Currency(code currency.CurrencyCode) *PaymentRequestBuilder {
+	p.PaymentRequest.CurrencyCode = code
+	return p
+}
+func (p *PaymentRequestBuilder) Language(lCode string) *PaymentRequestBuilder {
+	p.PaymentRequest.LanguageCode = lCode
+	return p
+}
+func (p *PaymentRequestBuilder) ForOrder(oNumber string, oTotal float64) *PaymentRequestBuilder {
+
+	p.PaymentRequest.OrderNumber = oNumber
+	p.PaymentRequest.OrderTotal = oTotal
+	return p
+}
+func (p *PaymentRequestBuilder) WithInstallment(installment int) *PaymentRequestBuilder {
+	p.PaymentRequest.InstallmentCount = installment
+	return p
+}
+func (p *PaymentRequestBuilder) To(customer *Customer) *PaymentRequestBuilder {
+	p.PaymentRequest.Customer = customer
+	return p
+}
+func (p *PaymentRequestBuilder) InSuccessReturns(successUrl string) *PaymentRequestBuilder {
+	p.PaymentRequest.SuccessURL = successUrl
+	return p
+}
+
+func (p *PaymentRequestBuilder) InErrorReturns(failUrl string) *PaymentRequestBuilder {
+	p.PaymentRequest.FailURL = failUrl
+	return p
+}
+
+func (p *PaymentRequestBuilder) Build() *PaymentRequest {
+	return p.PaymentRequest
 }
