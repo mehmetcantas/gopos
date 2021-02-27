6c51a99b52a0a4c62e74bf47ffa4693c7c39639f
Mehmet Can Taş <tass.mehmetcan@outlook.com>
2021-02-27 21:34:46 +0300

    Add billing and shipping customer name to customer struct

--- models/customer.go
+++ models/customer.go
@@ -1,14 +1,15 @@
 package models
 
 type Customer struct {
-	FullName               string
 	EmailAddress           string
+	ShippingCustomerName   string
 	ShippingAddress        string
 	ShippingAddressZipCode string
+	BillingCustomerName    string
 	BillingAddress         string
 	BillingAddressZipCode  string
 	IPAddress              string
-	IsCompany              bool
+	IsBillingToCompany     bool
 	CustomerID             string
 }
 
@@ -20,22 +21,20 @@ func NewCustomerBuilder() *CustomerBuilder {
 	return &CustomerBuilder{&Customer{}}
 }
 
-func (c *CustomerBuilder) NameIs(name string) *CustomerBuilder {
-	c.customer.FullName = name
-	return c
-}
 func (c *CustomerBuilder) EmailIs(email string) *CustomerBuilder {
 	c.customer.EmailAddress = email
 	return c
 }
-func (c *CustomerBuilder) ShipTo(sAddress string, sZipCode string) *CustomerBuilder {
+func (c *CustomerBuilder) ShipTo(sName string, sAddress string, sZipCode string) *CustomerBuilder {
 	c.customer.ShippingAddress = sAddress
+	c.customer.ShippingCustomerName = sName
 	c.customer.ShippingAddressZipCode = sZipCode
 	return c
 }
 
-func (c *CustomerBuilder) BillTo(bAddress string, bZipCode string) *CustomerBuilder {
+func (c *CustomerBuilder) BillTo(bName string, bAddress string, bZipCode string) *CustomerBuilder {
 	c.customer.BillingAddress = bAddress
+	c.customer.BillingCustomerName = bName
 	c.customer.BillingAddressZipCode = bZipCode
 	return c
 }
@@ -43,11 +42,15 @@ func (c *CustomerBuilder) IpAddress(ipAddress string) *CustomerBuilder {
 	c.customer.IPAddress = ipAddress
 	return c
 }
-func (c *CustomerBuilder) IsCompany(isCompany bool) *CustomerBuilder {
-	c.customer.IsCompany = isCompany
+func (c *CustomerBuilder) IsBillingToCompany(isCompany bool) *CustomerBuilder {
+	c.customer.IsBillingToCompany = isCompany
 	return c
 }
 func (c *CustomerBuilder) WithID(id string) *CustomerBuilder {
 	c.customer.CustomerID = id
 	return c
+}
+
+func (c *CustomerBuilder) Build() *Customer {
+	return c.customer
 }
